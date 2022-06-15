98ab218fb9df307bdf3a33b23670b7e0a769fe30
Criku <isoblack1@outlook.com>
2022-06-15 13:49:35 +0800

    Initial Commit

---
+++ vChat/vChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  vChatApp.swift
+//  vChat
+//
+//  Created by 苏子轩 on 2022/6/15.
+//
+
+import SwiftUI
+
+@main
+struct vChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vChatUITests/vChatUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  vChatUITestsLaunchTests.swift
+//  vChatUITests
+//
+//  Created by 苏子轩 on 2022/6/15.
+//
+
+import XCTest
+
+class vChatUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
