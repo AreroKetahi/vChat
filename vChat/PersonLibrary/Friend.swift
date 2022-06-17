208184ac0d27265e86089b2bf529a256c1f71ee8
Criku <isoblack1@outlook.com>
2022-06-17 20:23:36 +0800

    Add friend file and person struct.

---
+++ vChat/PersonLibrary/Friend.swift
@@ -0,0 +1,14 @@
+//
+//  Friend.swift
+//  vChat
+//
+//  Created by 苏子轩 on 2022/6/17.
+//
+
+import Foundation
+
+struct Person {
+    var nickname: String
+    var uid: UInt
+    var publicKey: NSObject
+}
