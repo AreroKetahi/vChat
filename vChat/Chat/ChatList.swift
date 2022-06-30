ae0ff9d9942aff40cdb2d8b470ca075d22c7bf08
Criku <isoblack1@outlook.com>
2022-06-30 18:23:21 +0800

    Modify structure.

--- vChat/Chat/ChatList.swift
+++ vChat/Chat/ChatList.swift
@@ -10,31 +10,31 @@ import SwiftUI
 struct ChatList: View {
     var body: some View {
 //        NavigationView {//Need to notes this code while compiling
-            List {
-                NavigationLink {
-                    ChatPage()
-                } label: {
-                    HStack {
-                    Image("TestImage")
-                    
-                        .resizable()
-                        .frame(width: 49, height: 49)
-                        .cornerRadius(8)
-                        .shadow(color: Color(CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3)),
-                                radius: 3, x: 0, y: 0)
+        List {
+            NavigationLink {
+                ChatPage()
+            } label: {
+                HStack {
+                Image("TestImage")
+                
+                    .resizable()
+                    .frame(width: 49, height: 49)
+                    .cornerRadius(8)
+                    .shadow(color: Color(CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3)),
+                            radius: 3, x: 0, y: 0)
 
-                    Text("Zhang San")
-                        .foregroundColor(.black)
-                        .shadow(color: Color(CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3)),
-                                radius: 1, x: 0, y: 0)
-                    }
-//                    .overlay(Circle().frame(width: 12, height: 12).position(x: 47, y: 3).foregroundColor(.blue).shadow(color: .blue, radius: 2)) //Notification Point
-                    
+                Text("Zhang San")
+                    .foregroundColor(.black)
+                    .shadow(color: Color(CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3)),
+                            radius: 1, x: 0, y: 0)
                 }
+                    .overlay(Circle().frame(width: 12, height: 12).position(x: 47, y: 3).foregroundColor(.blue).shadow(color: .blue, radius: 2)) //Notification Point
+                    
             }
-            .navigationTitle("Chat")
-            .navigationBarTitleDisplayMode(.inline)
-//        } //Need to notes this code while compiling
+        }
+        .navigationTitle("Chat")
+        .navigationBarTitleDisplayMode(.inline)
+//      } //Need to notes this code while compiling
     }
 }
 
