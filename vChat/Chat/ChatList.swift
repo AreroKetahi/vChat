421a3a90188783695702c2327e610236c8186b38
Criku <isoblack1@outlook.com>
2022-06-30 17:51:34 +0800

    Delete some unnecessary code. Modify details.

--- vChat/Chat/ChatList.swift
+++ vChat/Chat/ChatList.swift
@@ -9,26 +9,32 @@ import SwiftUI
 
 struct ChatList: View {
     var body: some View {
-        NavigationView {
+//        NavigationView {//Need to notes this code while compiling
             List {
                 NavigationLink {
                     ChatPage()
                 } label: {
+                    HStack {
                     Image("TestImage")
+                    
                         .resizable()
-                        .frame(width: 40, height: 40)
-                        .cornerRadius(3)
+                        .frame(width: 49, height: 49)
+                        .cornerRadius(8)
                         .shadow(color: Color(CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3)),
                                 radius: 3, x: 0, y: 0)
+
                     Text("Zhang San")
+                        .foregroundColor(.black)
                         .shadow(color: Color(CGColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 0.3)),
-                                radius: 3, x: 0, y: 0)
+                                radius: 1, x: 0, y: 0)
+                    }
+//                    .overlay(Circle().frame(width: 12, height: 12).position(x: 47, y: 3).foregroundColor(.blue).shadow(color: .blue, radius: 2)) //Notification Point
+                    
                 }
             }
             .navigationTitle("Chat")
             .navigationBarTitleDisplayMode(.inline)
-//            .navigationBarHidden(true)
-        }
+//        } //Need to notes this code while compiling
     }
 }
 
