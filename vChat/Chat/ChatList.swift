74f523f1a81cdef580d72d5de874bcbf4faee387
Criku <isoblack1@outlook.com>
2022-08-05 18:08:19 +0800

    Enable global accent color changing. Added back NavigationTitle and fixed previous bug.

--- vChat/Chat/ChatList.swift
+++ vChat/Chat/ChatList.swift
@@ -10,22 +10,27 @@ import SwiftUI
 struct ChatList: View {
     var friendList: [Person]
     
+    @Binding var uiColor: Color
+    @Binding var navigationTitle: String
     @Environment(\.colorScheme) private var colorScheme
     var body: some View {
 //        NavigationView {//Need to notes this code while compiling
         List(friendList) { friend in
             NavigationLink {
-                ChatPage(friendImageName: friend.imageName, selfImageName: selfImageName, objectsVars: VCEnvironmentObjects())
+                ChatPage(friendImageName: friend.imageName, selfImageName: selfImageName, objectsVars: VCEnvironmentObjects(), uiColor: $uiColor)
                     .navigationBarTitle(friend.remark ?? friend.nickname)
             } label: {
                 PersonLabelComponent(imageName: friend.imageName, nickname: friend.remark ?? friend.nickname)
                 .overlay(Circle()
                     .frame(width: 12, height: 12)
                     .position(x: 47, y: 3)
-                    .foregroundColor(.blue)
-                    .shadow(color: .blue, radius: 2)
+                    .foregroundColor(uiColor)
+                    .shadow(color: uiColor, radius: 2)
                 ) //Notification Point
             }
+        }
+        .onAppear {
+            navigationTitle = NSLocalizedString("ChatList.Chat", comment: "super:ChatList")
         }
 //        .navigationTitle("Chat")
         .navigationBarTitleDisplayMode(.inline)
@@ -36,7 +41,7 @@ struct ChatList: View {
 struct ChatList_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            ChatList(friendList: friendList)
+            ChatList(friendList: friendList, uiColor: .constant(.blue), navigationTitle: .constant("Chat"))
         }
     }
 }
